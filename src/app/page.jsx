fb0d9da93602e2071aa1d9748ce7c6beffba28ef
Tito <drammatik@gmail.com>
2025-01-31 11:47:36 +0000

    adding maintain mode for now until i am done with development for the actual portfiolo

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,31 @@
 
-import SharedLayout from './components/SharedLayout';
+// import SharedLayout from './components/SharedLayout';
 
+
+// export default function Home() {
+//   const menuLinks = [
+//     { href: '/', label: 'Home' },
+//     { href: '#', label: 'Github' },
+//     { href: '#', label: 'Contact' },
+//   ];
+
+//   const introText = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquid cum distinctio fuga inventore sit rem dignissimos possimus optio, voluptatum sunt voluptatem ad facere hic nostrum cumque ducimus voluptates consequatur iure.'
+//   const defaultAudioSrc = '/audio/TownshipWinds.mp3'
+
+
+
+//   return (
+//     <SharedLayout
+//       bgImage={'/happyme.jpg'}
+//       menuLinks={menuLinks}
+//       content={introText}
+//       audioSrc={defaultAudioSrc}
+//       />
+//   );
+// }
+
+import MaintainceLandingPage from '@/app/components/MaintainceLandingPage';
 
 export default function Home() {
-  const menuLinks = [
-    { href: '/', label: 'Home' },
-    { href: '#', label: 'Github' },
-    { href: '#', label: 'Contact' },
-  ];
-
-  const introText = 'I&#44;m a front-end developer with over a decade of experience crafting digital solutions in tech-driven environments. Specializing in React, TypeScript, HTML/CSS, and JavaScript. I&#44;ve delivered impactful results to clients across diverse sectors. Known for my creative approach to troubleshooting, I bring a unique blend of analytical prowess and artistic intuition to the tech landscape.'
-  const defaultAudioSrc = '/audio/TownshipWinds.mp3'
-
-
-
-  return (
-    <SharedLayout
-      bgImage={'/happyme.jpg'}
-      menuLinks={menuLinks}
-      content={introText}
-      audioSrc={defaultAudioSrc}
-      />
-  );
+  return <MaintainceLandingPage />;
 }
